742d22175cf7a203f9d5dbfaf75fd73f39d830a6
kumbhto <MrunaliniKhushal.Kumbhalkar@tiaa.org>
2020-12-28 19:53:46 -0500

    no message

--- Sources/SwiftUIImagePackage/ImageFetcherView.swift
+++ Sources/SwiftUIImagePackage/ImageFetcherView.swift
@@ -62,7 +62,7 @@ public struct MoviewView: View {
             if let image = image, let title = title {
                 Image(uiImage: image)
                     .resizable()
-                    .aspectRatio(CGSize(width: 10, height: 10), contentMode: .fit)
+                    .aspectRatio(CGSize(width: UIScreen.main.bounds.width, height: 10), contentMode: .fit)
                 Text(verbatim: title)
                     .bold()
             }
