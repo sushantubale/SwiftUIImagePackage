d38bd33185784094c8f0a7a65c154ea9b3c3c752
kumbhto <MrunaliniKhushal.Kumbhalkar@tiaa.org>
2020-12-30 21:47:11 -0500

    no message

--- Sources/SwiftUIImagePackage/ImageFetcherView.swift
+++ Sources/SwiftUIImagePackage/ImageFetcherView.swift
@@ -64,7 +64,7 @@ public struct MoviewView: View {
                     .renderingMode(.original)
                     .resizable()
                        .scaledToFit()
-                       .clipShape(Rectangle())
+                    .clipShape(RoundedRectangle(cornerRadius: 1.0, style: RoundedCornerStyle.continuous))
                        .shadow(color: Color.blue.opacity(5.0), radius: 5, x: 5, y: 5)
                 Text(verbatim: title)
                     .bold()
